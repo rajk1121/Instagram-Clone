1b3e27553b5e6b4303f34f88a16e629b0ff86582
rajk1121 <rajat.mishra1121@gmail.com>
2020-10-04 13:34:10 +0530

    navbar added

--- app.js
+++ app.js
@@ -23,13 +23,13 @@ app.use(express.json())
 app.use('/user/', userRouter)
 app.use("/auth/", authRouter)
 app.use('/post/', postRouter)
-if(process.env.NODE_ENV=="production"){
+// if(process.env.NODE_ENV=="production"){
     const path = require('path')
     app.use(express.static('client/build'))
     app.get("*",(req,res)=>{
         res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'))
     })
-}
+// }
 app.listen(PORT, ()=>{
     console.log("Server running at port ", PORT)
 })
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,8 +3,15 @@ import {Link} from 'react-router-dom'
 import {UserContext} from '../App'
 import {useHistory} from 'react-router-dom'
 import SearchModal from './searchModal'
+import M from 'materialize-css'
 const Navbar = ()=>{
   const history = useHistory()
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+  });
+
   const {state, dispatch} = useContext(UserContext)
   console.log(state)
   const logout = ()=>{
@@ -14,19 +21,31 @@ const Navbar = ()=>{
   }
     if(state){
       return (
-        <nav>
-        <div className="nav-wrapper">
-          <Link to="#" className="brand-logo left">Instagram</Link>
-          <ul id="nav-mobile" className="right">
-            <li><SearchModal></SearchModal></li>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/profile">Profile</Link></li>
-            <button className="btn waves-effect red darken-1" onClick={logout}>LogOut
-            </button>
-            <li><Link to="/createPost">Create Post</Link></li>
+        <div>
+          <nav>
+              <div className="nav-wrapper">
+                <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                
+                  <Link to="/" className="brand-logo left hide-on-med-and-down">Instagram</Link>
+                <ul id="" className="right hide-on-med-and-down">
+                  <li><SearchModal></SearchModal></li>
+                  <li><Link to="/">Home</Link></li>
+                  <li><Link to="/profile">Profile</Link></li>
+                  <button className="btn waves-effect red darken-1" onClick={logout}>LogOut
+                  </button>
+                  <li><Link to="/createPost">Create Post</Link></li>
+                </ul>
+              </div>
+            </nav>
+            <ul class="sidenav" id="mobile-demo">
+              <li><SearchModal></SearchModal></li>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/profile">Profile</Link></li>
+              <li><Link to="/createPost">Create Post</Link></li>
+              <li><button className="btn waves-effect red darken-1"  style={{marginLeft : "2rem"}} onClick={logout}>LogOut
+                    </button></li>
           </ul>
         </div>
-      </nav>
             
     )
     }else{
